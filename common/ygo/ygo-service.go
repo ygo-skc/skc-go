0ab51f18d01add99a851582c41ddee0dd695c1b2
Javi <8519394+rtomyj@users.noreply.github.com>
2025-05-14 22:41:00 -0500

    Added client creator utility

---
+++ common/ygo/ygo-service.go
@@ -0,0 +1,53 @@
+package ygo
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log/slog"
+	"time"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/keepalive"
+)
+
+func CreateCardServiceClient(sslServerName string, serviceHost string) (*CardServiceClient, error) {
+	slog.Info(fmt.Sprintf("Creating Card Service gRPC Client using SSL Server Name %s and Host %s",
+		sslServerName,
+		serviceHost))
+
+	creds := credentials.NewTLS(&tls.Config{
+		InsecureSkipVerify: false,
+		ServerName:         sslServerName,
+	})
+
+	conn, err := grpc.NewClient(serviceHost,
+		grpc.WithTransportCredentials(creds),
+		grpc.WithDefaultCallOptions(
+			grpc.UseCompressor("gzip"),
+		),
+		grpc.WithDefaultServiceConfig(`{
+			"methodConfig": [{
+				"name": [{"service": "ygo.CardService"}],
+				"retryPolicy": {
+					"MaxAttempts": 3,
+					"InitialBackoff": "0.1s",
+					"MaxBackoff": "1s",
+					"BackoffMultiplier": 2.0,
+					"RetryableStatusCodes": ["UNAVAILABLE", "DEADLINE_EXCEEDED"]
+				}
+			}]
+		}`),
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:                30 * time.Second,
+			Timeout:             1 * time.Second,
+			PermitWithoutStream: true,
+		}))
+
+	if err != nil {
+		return nil, err
+	}
+
+	CardServiceClient := NewCardServiceClient(conn)
+	return &CardServiceClient, nil
+}
