9074bee6dbe990ab1d74ea317ef372be9433494e
Javi <8519394+rtomyj@users.noreply.github.com>
2025-05-21 22:12:57 -0500

    Added handler for GetCardsByName - updated names as per proto changes

--- ygo-service/api/card_service_handler.go
+++ ygo-service/api/card_service_handler.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func (s *ygoServiceServer) Colors(ctx context.Context, req *emptypb.Empty) (*ygo.CardColors, error) {
+func (s *ygoServiceServer) GetCardColors(ctx context.Context, req *emptypb.Empty) (*ygo.CardColors, error) {
 	logger, ctx := util.NewRequestSetup(ctx, "Card Colors")
 	logger.Info("Retrieving card colors")
 
@@ -23,8 +23,8 @@ func (s *ygoServiceServer) Colors(ctx context.Context, req *emptypb.Empty) (*ygo
 	}
 }
 
-func (s *ygoServiceServer) QueryCard(ctx context.Context, req *ygo.Resource) (*ygo.Card, error) {
-	logger, ctx := util.NewRequestSetup(ctx, "Query Card")
+func (s *ygoServiceServer) GetCardByID(ctx context.Context, req *ygo.ResourceID) (*ygo.Card, error) {
+	logger, ctx := util.NewRequestSetup(ctx, "Query Card By ID")
 	logger.Info(fmt.Sprintf("Fetching card details using card ID: %v", req.ID))
 
 	if c, err := skcDBInterface.GetCardByID(ctx, req.ID); err != nil && err.StatusCode == http.StatusNotFound {
@@ -36,8 +36,8 @@ func (s *ygoServiceServer) QueryCard(ctx context.Context, req *ygo.Resource) (*y
 	}
 }
 
-func (s *ygoServiceServer) QueryCards(ctx context.Context, req *ygo.Resources) (*ygo.Cards, error) {
-	logger, ctx := util.NewRequestSetup(ctx, "Query Cards")
+func (s *ygoServiceServer) GetCardsByID(ctx context.Context, req *ygo.ResourceIDs) (*ygo.Cards, error) {
+	logger, ctx := util.NewRequestSetup(ctx, "Query Cards By ID")
 	logger.Info(fmt.Sprintf("Fetching card details using card ID's: %v", req.IDs))
 
 	if cards, err := skcDBInterface.GetCardsByIDs(ctx, req.IDs); err != nil {
@@ -48,7 +48,19 @@ func (s *ygoServiceServer) QueryCards(ctx context.Context, req *ygo.Resources) (
 	}
 }
 
-func (s *ygoServiceServer) RandomCard(ctx context.Context, req *ygo.BlackListedResources) (*ygo.Card, error) {
+func (s *ygoServiceServer) GetCardsByName(ctx context.Context, req *ygo.ResourceNames) (*ygo.Cards, error) {
+	logger, ctx := util.NewRequestSetup(ctx, "Query Cards By Name")
+	logger.Info(fmt.Sprintf("Fetching card details using %d card name(s)", len(req.Names)))
+
+	if cards, err := skcDBInterface.GetCardsByNames(ctx, req.Names); err != nil {
+		return nil, status.Errorf(codes.Internal, "%s", err.Message)
+	} else {
+		logger.Info(fmt.Sprintf("The following cards were not found: %v", cards.UnknownResources))
+		return cards, nil
+	}
+}
+
+func (s *ygoServiceServer) GetRandomCard(ctx context.Context, req *ygo.BlackListed) (*ygo.Card, error) {
 	logger, ctx := util.NewRequestSetup(ctx, "Random Card")
 	logger.Info(fmt.Sprintf("Getting random card from DB. Client has provided %d blacklisted IDs", len(req.BlackListedRefs)))
 
