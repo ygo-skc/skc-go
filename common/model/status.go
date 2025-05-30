c62beebec417c9e1579800a1f76db85bd4152c31
Javi <8519394+rtomyj@users.noreply.github.com>
2025-05-29 21:04:41 -0500

    Added version field

--- common/model/status.go
+++ common/model/status.go
@@ -7,6 +7,7 @@ type APIHealth struct {
 
 type DownstreamItem struct {
 	ServiceName string `json:"serviceName"`
+	Version     string `json:"version,omitempty"`
 	Status      Status `json:"status"`
 }
 
