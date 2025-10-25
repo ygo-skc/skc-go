04ef381e2db7b39330a829c1a4e24a62026ccdec
Javi <8519394+rtomyj@users.noreply.github.com>
2025-10-25 10:34:17 -0500

    Updated version

--- ygo-service/api/status_handler.go
+++ ygo-service/api/status_handler.go
@@ -12,5 +12,5 @@ func (s *healthServiceServer) APIStatus(ctx context.Context, req *emptypb.Empty)
 	logger, _ := util.NewLogger(context.Background(), "Status")
 	logger.Info("Retrieving status of gRPC service")
 
-	return &health.APIStatusDetails{Version: "2.0.0"}, nil
+	return &health.APIStatusDetails{Version: "2.1.0"}, nil
 }
