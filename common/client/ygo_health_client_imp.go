ac7c48e3b22e9ff17a436f21fa1fd4d1fbc8b28b
Javi <8519394+rtomyj@users.noreply.github.com>
2026-07-20 21:31:38 -0500

    Utilizing util

--- common/client/ygo_health_client_imp.go
+++ common/client/ygo_health_client_imp.go
@@ -3,7 +3,6 @@ package client
 import (
 	context "context"
 	"log/slog"
-	"net/http"
 
 	"github.com/ygo-skc/skc-go/common/v2/health"
 	"github.com/ygo-skc/skc-go/common/v2/model"
@@ -21,11 +20,10 @@ type YGOHealthClientImpV1 struct {
 
 func (imp YGOHealthClientImpV1) GetAPIStatus(ctx context.Context) (*health.APIStatusDetails, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
-
-	if h, err := imp.client.APIStatus(ctx, &emptypb.Empty{}); err != nil {
+	h, err := imp.client.APIStatus(ctx, &emptypb.Empty{})
+	if err != nil {
 		logger.Error("Issue retrieving YGO Service status", slog.Any("err", err))
-		return nil, &model.APIError{Message: "API is down", StatusCode: http.StatusInternalServerError}
-	} else {
-		return h, nil
+		return nil, RPCErrorToAPIError(err)
 	}
+	return h, nil
 }
