76f05532d452a145245c8ade4ae677896b72f1f1
Javi <8519394+rtomyj@users.noreply.github.com>
2026-07-20 21:20:55 -0500

    Simplifying client code

--- common/client/ygo_card_client_imp.go
+++ common/client/ygo_card_client_imp.go
@@ -3,41 +3,23 @@ package client
 import (
 	context "context"
 	"log/slog"
-	"net/http"
 
 	"github.com/ygo-skc/skc-go/common/v2/model"
 	"github.com/ygo-skc/skc-go/common/v2/util"
 	"github.com/ygo-skc/skc-go/common/v2/ygo"
-	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type YGOCardClientImp interface {
 	GetCardColorsProto(context.Context) (*ygo.CardColors, *model.APIError)
-
 	GetCardByIDProto(context.Context, string) (*ygo.Card, *model.APIError)
-	GetCardByID(context.Context, string) (*model.YGOCard, *model.APIError)
-
 	GetCardsByIDProto(context.Context, model.CardIDs) (*ygo.Cards, *model.APIError)
-	GetCardsByID(context.Context, model.CardIDs) (*model.BatchCardData[model.CardIDs], *model.APIError)
-
 	GetCardsByNameProto(context.Context, model.CardNames) (*ygo.Cards, *model.APIError)
-	GetCardsByName(context.Context, model.CardNames) (*model.BatchCardData[model.CardNames], *model.APIError)
-
 	GetCardsReferencingNameInEffectProto(context.Context, []string) (*ygo.CardList, *model.APIError)
-	GetCardsReferencingNameInEffect(context.Context, []string) ([]model.YGOCard, *model.APIError)
-
 	GetArchetypalCardsUsingCardNameProto(context.Context, string) (*ygo.CardList, *model.APIError)
-	GetArchetypalCardsUsingCardName(context.Context, string) ([]model.YGOCard, *model.APIError)
-
 	GetExplicitArchetypalInclusionsProto(context.Context, string) (*ygo.CardList, *model.APIError)
-	GetExplicitArchetypalInclusions(context.Context, string) ([]model.YGOCard, *model.APIError)
-
 	GetExplicitArchetypalExclusionsProto(context.Context, string) (*ygo.CardList, *model.APIError)
-	GetExplicitArchetypalExclusions(context.Context, string) ([]model.YGOCard, *model.APIError)
-
 	GetRandomCardProto(context.Context, []string) (*ygo.Card, *model.APIError)
-	GetRandomCard(context.Context, []string) (*model.YGOCard, *model.APIError)
 }
 type YGOCardClientImpV1 struct {
 	client ygo.CardServiceClient
@@ -46,228 +28,110 @@ type YGOCardClientImpV1 struct {
 func (imp YGOCardClientImpV1) GetCardColorsProto(ctx context.Context) (*ygo.CardColors, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Retrieving card colors")
-
-	if cColors, err := imp.client.GetCardColors(ctx, &emptypb.Empty{}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_card_colors"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching card color data", StatusCode: http.StatusInternalServerError}
-	} else {
-		return cColors, nil
+	cColors, err := imp.client.GetCardColors(ctx, &emptypb.Empty{})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_card_colors"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return cColors, nil
 }
 
 func (imp YGOCardClientImpV1) GetCardByIDProto(ctx context.Context, cardID string) (*ygo.Card, *model.APIError) {
-	return getCardByID(ctx, imp.client, cardID)
-}
-
-func (imp YGOCardClientImpV1) GetCardByID(ctx context.Context, cardID string) (*model.YGOCard, *model.APIError) {
-	c, err := getCardByID(ctx, imp.client, cardID)
-	if err == nil {
-		card := model.YGOCardRESTFromProto(c)
-		return &card, nil
-	}
-	return nil, err
-}
-
-func getCardByID(ctx context.Context, client ygo.CardServiceClient, cardID string) (*ygo.Card, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Fetching card info", slog.String("card_id", cardID))
-
-	if cards, err := client.GetCardByID(ctx, &ygo.ResourceID{ID: cardID}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_card_by_id"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching card info", StatusCode: http.StatusInternalServerError}
-	} else {
-		return cards, nil
+	card, err := imp.client.GetCardByID(ctx, &ygo.ResourceID{ID: cardID})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_card_by_id"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return card, nil
 }
 
 func (imp YGOCardClientImpV1) GetCardsByIDProto(ctx context.Context, cardIDs model.CardIDs) (*ygo.Cards, *model.APIError) {
-	return getCardsByID(ctx, imp.client, cardIDs)
-}
-
-func (imp YGOCardClientImpV1) GetCardsByID(ctx context.Context, cardIDs model.CardIDs) (*model.BatchCardData[model.CardIDs], *model.APIError) {
-	c, err := getCardsByID(ctx, imp.client, cardIDs)
-	if err == nil {
-		return model.BatchCardDataFromProto[model.CardIDs](c, model.CardIDAsKey), nil
-	}
-	return nil, err
-}
-
-func getCardsByID(ctx context.Context, client ygo.CardServiceClient, cardIDs model.CardIDs) (*ygo.Cards, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Fetching card info", slog.Any("card_ids", cardIDs))
-
-	if cards, err := client.GetCardsByID(ctx, &ygo.ResourceIDs{IDs: cardIDs}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_cards_by_id"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching batch card info", StatusCode: http.StatusInternalServerError}
-	} else {
-		if cards.UnknownResources == nil {
-			cards.UnknownResources = make([]string, 0)
-		}
-		return cards, nil
+	cards, err := imp.client.GetCardsByID(ctx, &ygo.ResourceIDs{IDs: cardIDs})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_cards_by_id"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	if cards.UnknownResources == nil {
+		cards.UnknownResources = make([]string, 0)
+	}
+	return cards, nil
 }
 
 func (imp YGOCardClientImpV1) GetCardsByNameProto(ctx context.Context, cardNames model.CardNames) (*ygo.Cards, *model.APIError) {
-	return getCardsByName(ctx, imp.client, cardNames)
-}
-
-func (imp YGOCardClientImpV1) GetCardsByName(ctx context.Context, cardNames model.CardNames) (*model.BatchCardData[model.CardNames], *model.APIError) {
-	c, err := getCardsByName(ctx, imp.client, cardNames)
-	if err == nil {
-		return model.BatchCardDataFromProto[model.CardNames](c, model.CardNameAsKey), nil
-	}
-	return nil, err
-}
-
-func getCardsByName(ctx context.Context, client ygo.CardServiceClient, cardNames model.CardNames) (*ygo.Cards, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Fetching card info", slog.Int("card_name_count", len(cardNames)))
-
-	if cards, err := client.GetCardsByName(ctx, &ygo.ResourceNames{Names: cardNames}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_cards_by_name"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching batch card info", StatusCode: http.StatusInternalServerError}
-	} else {
-		if cards.UnknownResources == nil {
-			cards.UnknownResources = make([]string, 0)
-		}
-		return cards, nil
+	cards, err := imp.client.GetCardsByName(ctx, &ygo.ResourceNames{Names: cardNames})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_cards_by_name"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	if cards.UnknownResources == nil {
+		cards.UnknownResources = make([]string, 0)
+	}
+	return cards, nil
 }
 
 func (imp YGOCardClientImpV1) GetCardsReferencingNameInEffectProto(ctx context.Context, namesOfCards []string) (*ygo.CardList, *model.APIError) {
-	return getCardsReferencingNameInEffect(ctx, imp.client, namesOfCards)
-}
-
-func (imp YGOCardClientImpV1) GetCardsReferencingNameInEffect(ctx context.Context, namesOfCards []string) ([]model.YGOCard, *model.APIError) {
-	c, err := getCardsReferencingNameInEffect(ctx, imp.client, namesOfCards)
-	if err == nil {
-		return model.YGOCardListRESTFromProto(c), nil
-	}
-	return nil, err
-}
-
-func getCardsReferencingNameInEffect(ctx context.Context, client ygo.CardServiceClient, namesOfCards []string) (*ygo.CardList, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Fetching cards referencing names in card text", slog.Any("names", namesOfCards))
-
-	if cards, err := client.GetCardsReferencingNameInEffect(ctx, &ygo.ResourceNames{Names: namesOfCards}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "search_card_references_using_text"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error searching card text for references", StatusCode: http.StatusInternalServerError}
-	} else {
-		return cards, nil
+	cards, err := imp.client.GetCardsReferencingNameInEffect(ctx, &ygo.ResourceNames{Names: namesOfCards})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "search_card_references_using_text"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return cards, nil
 }
 
 /*
 Archetype functionality
 */
 func (imp YGOCardClientImpV1) GetArchetypalCardsUsingCardNameProto(ctx context.Context, archetype string) (*ygo.CardList, *model.APIError) {
-	return getArchetypalCardsUsingCardName(ctx, imp.client, archetype)
-}
-
-func (imp YGOCardClientImpV1) GetArchetypalCardsUsingCardName(ctx context.Context, archetype string) ([]model.YGOCard, *model.APIError) {
-	c, err := getArchetypalCardsUsingCardName(ctx, imp.client, archetype)
-	if err == nil {
-		return model.YGOCardListRESTFromProto(c), nil
-	}
-	return nil, err
-}
-
-func getArchetypalCardsUsingCardName(ctx context.Context, client ygo.CardServiceClient,
-	archetype string) (*ygo.CardList, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Fetching archetypal cards", slog.String("archetype", archetype))
-
-	if cards, err := client.GetArchetypalCardsUsingCardName(ctx, &ygo.Archetype{Archetype: archetype}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_archetypal_cards_using_name"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching archetypal data", StatusCode: http.StatusInternalServerError}
-	} else {
-		return cards, nil
+	cards, err := imp.client.GetArchetypalCardsUsingCardName(ctx, &ygo.Archetype{Archetype: archetype})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_archetypal_cards_using_name"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return cards, nil
 }
 
 func (imp YGOCardClientImpV1) GetExplicitArchetypalInclusionsProto(ctx context.Context, archetype string) (*ygo.CardList, *model.APIError) {
-	return getExplicitArchetypalInclusions(ctx, imp.client, archetype)
-}
-
-func (imp YGOCardClientImpV1) GetExplicitArchetypalInclusions(ctx context.Context, archetype string) ([]model.YGOCard, *model.APIError) {
-	c, err := getExplicitArchetypalInclusions(ctx, imp.client, archetype)
-	if err == nil {
-		return model.YGOCardListRESTFromProto(c), nil
-	}
-	return nil, err
-}
-
-func getExplicitArchetypalInclusions(ctx context.Context, client ygo.CardServiceClient, archetype string) (*ygo.CardList, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Fetching explicit archetype inclusions", slog.String("archetype", archetype))
-
-	if cards, err := client.GetExplicitArchetypalInclusions(ctx, &ygo.Archetype{Archetype: archetype}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_explicit_archetype_inclusions"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching explicit archetype inclusions", StatusCode: http.StatusInternalServerError}
-	} else {
-		return cards, nil
+	cards, err := imp.client.GetExplicitArchetypalInclusions(ctx, &ygo.Archetype{Archetype: archetype})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_explicit_archetype_inclusions"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return cards, nil
 }
 
 func (imp YGOCardClientImpV1) GetExplicitArchetypalExclusionsProto(ctx context.Context, archetype string) (*ygo.CardList, *model.APIError) {
-	return getExplicitArchetypalExclusions(ctx, imp.client, archetype)
-}
-
-func (imp YGOCardClientImpV1) GetExplicitArchetypalExclusions(ctx context.Context, archetype string) ([]model.YGOCard, *model.APIError) {
-	c, err := getExplicitArchetypalExclusions(ctx, imp.client, archetype)
-	if err == nil {
-		return model.YGOCardListRESTFromProto(c), nil
-	}
-	return nil, err
-}
-
-func getExplicitArchetypalExclusions(ctx context.Context, client ygo.CardServiceClient, archetype string) (*ygo.CardList, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Fetching explicit archetype exclusions", slog.String("archetype", archetype))
-
-	if cards, err := client.GetExplicitArchetypalExclusions(ctx, &ygo.Archetype{Archetype: archetype}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_explicit_archetype_exclusions"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching explicit archetype exclusions", StatusCode: http.StatusInternalServerError}
-	} else {
-		return cards, nil
+	cards, err := imp.client.GetExplicitArchetypalExclusions(ctx, &ygo.Archetype{Archetype: archetype})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_explicit_archetype_exclusions"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return cards, nil
 }
 
 /*
 Random card functionality
 */
 func (imp YGOCardClientImpV1) GetRandomCardProto(ctx context.Context, blackListedIDs []string) (*ygo.Card, *model.APIError) {
-	return getRandomCard(ctx, imp.client, blackListedIDs)
-}
-
-func (imp YGOCardClientImpV1) GetRandomCard(ctx context.Context, blackListedIDs []string) (*model.YGOCard, *model.APIError) {
-	c, err := getRandomCard(ctx, imp.client, blackListedIDs)
-	if err == nil {
-		card := model.YGOCardRESTFromProto(c)
-		return &card, nil
-	}
-	return nil, err
-}
-
-func getRandomCard(ctx context.Context,
-	client ygo.CardServiceClient, blackListedIDs []string) (*ygo.Card, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Getting random card")
-
-	if card, err := client.GetRandomCard(ctx, &ygo.BlackListed{BlackListedRefs: blackListedIDs}); err != nil {
-		logger.Error("Issue calling YGO Card Service",
-			slog.String("operation", "get_random_card"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: "Error fetching random card", StatusCode: http.StatusInternalServerError}
-	} else {
-		return card, nil
+	card, err := imp.client.GetRandomCard(ctx, &ygo.BlackListed{BlackListedRefs: blackListedIDs})
+	if err != nil {
+		logger.Error("Issue calling YGO Card Service", slog.String("operation", "get_random_card"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return card, nil
 }
--- common/client/ygo_product_client_imp.go
+++ common/client/ygo_product_client_imp.go
@@ -2,84 +2,51 @@ package client
 
 import (
 	context "context"
-	"fmt"
 	"log/slog"
-	"net/http"
 
 	"github.com/ygo-skc/skc-go/common/v2/model"
 	"github.com/ygo-skc/skc-go/common/v2/util"
 	"github.com/ygo-skc/skc-go/common/v2/ygo"
-	"google.golang.org/grpc/status"
 )
 
 type YGOProductClientImp interface {
 	GetCardsByProductIDProto(context.Context, string) (*ygo.Product, *model.APIError)
-
 	GetProductSummaryByIDProto(context.Context, string) (*ygo.ProductSummary, *model.APIError)
-
 	GetProductsSummaryByIDProto(context.Context, model.ProductIDs) (*ygo.Products, *model.APIError)
-	GetProductsSummaryByID(context.Context, model.ProductIDs) (*model.BatchProductSummaryData[model.ProductIDs], *model.APIError)
 }
 type YGOProductClientImpV1 struct {
 	client ygo.ProductServiceClient
 }
 
 func (imp YGOProductClientImpV1) GetCardsByProductIDProto(ctx context.Context, productID string) (*ygo.Product, *model.APIError) {
-	return getCardsByProductID(ctx, imp.client, productID)
-}
-
-func getCardsByProductID(ctx context.Context, productServiceClient ygo.ProductServiceClient, productID string) (*ygo.Product, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Retrieving cards for product", slog.String("product_id", productID))
-
-	if p, err := productServiceClient.GetCardsByProductID(ctx, &ygo.ResourceID{ID: productID}); err != nil {
-		logger.Error("Issue calling YGO Product Service",
-			slog.String("operation", "get_cards_by_product"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: fmt.Sprintf("Error fetching cards for product %s", productID), StatusCode: http.StatusInternalServerError}
-	} else {
-		return p, nil
+	p, err := imp.client.GetCardsByProductID(ctx, &ygo.ResourceID{ID: productID})
+	if err != nil {
+		logger.Error("Issue calling YGO Product Service", slog.String("operation", "get_cards_by_product"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return p, nil
 }
 
 func (imp YGOProductClientImpV1) GetProductSummaryByIDProto(ctx context.Context, productID string) (*ygo.ProductSummary, *model.APIError) {
-	return getProductSummaryByID(ctx, imp.client, productID)
-}
-
-func getProductSummaryByID(ctx context.Context, productServiceClient ygo.ProductServiceClient, productID string) (*ygo.ProductSummary, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Retrieving product summary", slog.String("product_id", productID))
-
-	if ps, err := productServiceClient.GetProductSummaryByID(ctx, &ygo.ResourceID{ID: productID}); err != nil {
-		logger.Error("Issue calling YGO Product Service",
-			slog.String("operation", "get_product_summary"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: fmt.Sprintf("Error fetching product summary for product %s", productID), StatusCode: http.StatusInternalServerError}
-	} else {
-		return ps, nil
+	ps, err := imp.client.GetProductSummaryByID(ctx, &ygo.ResourceID{ID: productID})
+	if err != nil {
+		logger.Error("Issue calling YGO Product Service", slog.String("operation", "get_product_summary"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return ps, nil
 }
 
-func (imp YGOProductClientImpV1) GetProductsSummaryByIDProto(ctx context.Context, productID model.ProductIDs) (*ygo.Products, *model.APIError) {
-	return getProductsSummaryByID(ctx, imp.client, productID)
-}
-
-func (imp YGOProductClientImpV1) GetProductsSummaryByID(ctx context.Context,
-	productID model.ProductIDs) (*model.BatchProductSummaryData[model.ProductIDs], *model.APIError) {
-	p, err := getProductsSummaryByID(ctx, imp.client, productID)
-	if err == nil {
-		return model.BatchProductSummaryFromProductsProto(p, model.ProductIDAsKey), nil
-	}
-	return nil, err
-}
-
-func getProductsSummaryByID(ctx context.Context, productServiceClient ygo.ProductServiceClient, productIDs model.ProductIDs) (*ygo.Products, *model.APIError) {
+func (imp YGOProductClientImpV1) GetProductsSummaryByIDProto(ctx context.Context, productIDs model.ProductIDs) (*ygo.Products, *model.APIError) {
 	logger := util.RetrieveLogger(ctx)
 	logger.Info("Retrieving product summaries", slog.Any("product_ids", productIDs))
-
-	if ps, err := productServiceClient.GetProductsSummaryByID(ctx, &ygo.ResourceIDs{IDs: productIDs}); err != nil {
-		logger.Error("Issue calling YGO Product Service",
-			slog.String("operation", "get_products_summary"), slog.Any("grpc_code", status.Code(err)), slog.Any("err", err))
-		return nil, &model.APIError{Message: fmt.Sprintf("Error fetching product summary for product(s) %v", productIDs), StatusCode: http.StatusInternalServerError}
-	} else {
-		return ps, nil
+	ps, err := imp.client.GetProductsSummaryByID(ctx, &ygo.ResourceIDs{IDs: productIDs})
+	if err != nil {
+		logger.Error("Issue calling YGO Product Service", slog.String("operation", "get_products_summary"), slog.Any("err", err))
+		return nil, RPCErrorToAPIError(err)
 	}
+	return ps, nil
 }
