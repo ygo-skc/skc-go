7494a11caa72c4ef43aec315cdc7069e39ddd9af
Javi <8519394+rtomyj@users.noreply.github.com>
2026-07-24 14:57:40 -0500

    Updated time logic

--- ygo-service/api/card_restriction_service_handler.go
+++ ygo-service/api/card_restriction_service_handler.go
@@ -24,12 +24,17 @@ func (s *ygoCardRestrictionServiceServer) GetEffectiveTimelineForFormat(ctx cont
 	if effectiveDates, err := cardRestrictionRepo.GetDatesForFormat(newCtx, format); err != nil {
 		return nil, err.Err()
 	} else {
-		today := time.Now().In(chicagoLocation).Truncate(24 * time.Hour)
+		now := time.Now().In(chicagoLocation)
+		today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, chicagoLocation)
 		futureDates := []string{}
 		var activeDate string
 
 		for _, effectiveDateStr := range effectiveDates {
-			effectiveDate, _ := time.Parse(effectiveDateStr, "2006-01-02")
+			effectiveDate, err := time.Parse("2006-01-02", effectiveDateStr)
+			if err != nil {
+				logger.Error("Failed to parse effective date", slog.String("effective_date", effectiveDateStr), slog.Any("err", err))
+				continue
+			}
 			if effectiveDate.After(today) {
 				futureDates = append(futureDates, effectiveDateStr)
 			} else {
