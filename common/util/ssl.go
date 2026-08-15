2d43983924639693fc84b78f1fe35a85594ed283
Javi <8519394+rtomyj@users.noreply.github.com>
2026-08-15 11:54:28 -0500

    Reading path more idiomatically

--- common/util/ssl.go
+++ common/util/ssl.go
@@ -1,20 +1,20 @@
 package util
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 )
 
 // combine certificate.crt and ca_bundle.crt to avoid issues with verifying the legitimacy of certificate.crt
 func CombineCerts(certDir string) {
-	if certData, err := os.ReadFile(fmt.Sprintf("%s/certificate.crt", certDir)); err != nil {
+	if certData, err := os.ReadFile(filepath.Join(certDir, "certificate.crt")); err != nil {
 		slog.Error("Failed to read certificate file", slog.String("cert_dir", certDir), slog.Any("err", err))
 		os.Exit(1)
-	} else if caBundleData, err := os.ReadFile(fmt.Sprintf("%s/ca_bundle.crt", certDir)); err != nil {
+	} else if caBundleData, err := os.ReadFile(filepath.Join(certDir, "ca_bundle.crt")); err != nil {
 		slog.Error("Failed to read CA bundle file", slog.String("cert_dir", certDir), slog.Any("err", err))
 		os.Exit(1)
-	} else if err = os.WriteFile(fmt.Sprintf("%s/concatenated.crt", certDir), append(certData, caBundleData...), 0600); err != nil {
+	} else if err = os.WriteFile(filepath.Join(certDir, "concatenated.crt"), append(certData, caBundleData...), 0600); err != nil {
 		slog.Error("Failed to write combined certificate file", slog.String("cert_dir", certDir), slog.Any("err", err))
 		os.Exit(1)
 	}
